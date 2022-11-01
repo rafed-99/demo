0b88b85c9e5a4b5c9469a3f9612182587fc8384a
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-11-02 00:46:27 +0100

    pom update

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
@@ -17,7 +16,7 @@ public class UserService implements IUserService{
         return userRepository.findAll();
     }
 
-    @Transactional
+    @Override
     public User addCategorieProduit(User u) {
         return userRepository.save(u);
     }
